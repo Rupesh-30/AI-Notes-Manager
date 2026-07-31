40ca07fe21b5c83fb8ff5afd3d945d00e8910ed5
Rupesh Das <rupesh30042004@gmail.com>
2026-07-31 09:37:24 +0530

    Export Firebase Functions

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -22,7 +22,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDVDVc_dXoD2eutEESdCK4t11lFqOc60Y8",
@@ -38,3 +38,4 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const functions = getFunctions(app, "asia-south2");
