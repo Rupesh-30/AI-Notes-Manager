14e71d621ed04d97856067f1a3ef1a8614164f51
Rupesh Das <rupesh30042004@gmail.com>
2026-07-31 17:21:49 +0530

    Check gemini env

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -1,5 +1,7 @@
 const { GoogleGenAI } = require("@google/genai");
 
+console.log("GEMINI KEY STATUS:", process.env.GEMINI_API_KEY ? "FOUND" : "MISSING");
+
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
